d6e2b9478853ae0ec4af254d357e03d831d79e7f
DESKTOP-A15V5NK\mmite <m.miter2002@gmail.com>
2022-09-28 21:14:11 +0200

    initial commit

---
+++ src/PierwszyProjekt.java
@@ -0,0 +1,10 @@
+public class PierwszyProjekt {
+
+    public static void main(String[] args) {
+        System.out.println("siemanko to ja");
+        System.out.println("ala ma kota");
+    }
+
+
+
+}
